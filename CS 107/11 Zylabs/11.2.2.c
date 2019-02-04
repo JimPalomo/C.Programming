4fb2c256ca4d5128472ef389016b5b69735d3a4d
JimPalomo <43157036+JimPalomo@users.noreply.github.com>
2019-02-04 10:23:44 -0600

    Update 11.2.2.c

--- CS 107/11 Zylabs/11.2.2.c
+++ CS 107/11 Zylabs/11.2.2.c
@@ -8,50 +8,54 @@
  int main () {
  
    int seedVal;
+   int numRolls;
    int die1;
    int die2;
    int die3;
    int total;
     
    // User input
-   printf("Please enter the number of dice to be rolled: \n");
    scanf("%d", &seedVal);
    srand(seedVal);
-
+   printf("Please enter the number of dice to be rolled: \n");
+   scanf("%d", &numRolls);
    
    // Dice roll & prompt
-   if (seedVal == 0) {
+   if (numRolls == 0) {
       printf("No Dice!\n");
       
    }
-   else if (seedVal == 1) {
+   else if (numRolls == 1) {
       die1 = ((rand() % 6) + 1);
       total = die1;
       printf("The one dice has a value of %d, for a total of %d\n", die1, total);
       
    }
-   else if (seedVal == 2) {
+   else if (numRolls == 2) {
    die1 = ((rand() % 6) + 1);
    die2 = ((rand() % 6) + 1);
    total = die1 + die2;
-   printf("The two dice has a value of %d and %d, for a total of %d\n", die1, die2, total);
+   printf("The two dice have values of %d and %d, for a total of %d\n", die1, die2, total);
    
    }
-   else if (seedVal == 3) {
+   else if (numRolls == 3) {
    die1 = ((rand() % 6) + 1);
    die2 = ((rand() % 6) + 1);
    die3 = ((rand() % 6) + 1);
    total = die1 + die2 + die3;
-   printf("The three dice has a value of %d, %d, and %d, for a total of %d\n", die1, die2, die3, total);   
+   printf("The three dice have values of %d, %d, and %d, for a total of %d\n", die1, die2, die3, total);   
+   
    }
-   else if (seedVal > 3) {
+   else if (numRolls > 3) {
       printf("We can only roll at most 3 dice\n");
+   
    }
-   else if (seedVal < 3) {
-    printf("Numer of dice must be greater than 0\n");  
+   else if (numRolls < 3) {
+    printf("Number of dice must be greater than 0\n");  
+   
    }
 
-   
+
 return 0;
 
 }
