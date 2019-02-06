73011c8bc92d76e6be11be60a715d2b75db3b667
JimPalomo <43157036+JimPalomo@users.noreply.github.com>
2019-02-05 21:50:26 -0600

    Update 11.15.1.2.c

--- CS 107/Projects/11.15.1.2.c
+++ CS 107/Projects/11.15.1.2.c
@@ -1,5 +1,15 @@
+
+/** -----------------------------------------------
+* This program determines the availability of seats
+* through use of if-statements, pattern recognition, 
+* modular calculations
+* Class: CS 107, Spring 2019
+* Author: Jim Palomo
+* Date: Feb. 5, 2019
+* ------------------------------------------------- **/ 
+
 /* 
-      11.15.1: Project 1a - Finding Patterns in a Seating Chart
+         11.15.1: Project 1a - Finding Patterns in a Seating Chart
         
       Checks seat availability according to seating pattern on Zybooks
         ---------------------- Back of Classroom -----------------------
@@ -22,49 +32,51 @@
 
    int main () {
    
-   char  userChar;
+   char  userChar; 
    int   userInt;
    int   checkRow = 1;
 
-   printf("Enter the seat row letter: \n");
+   printf("Enter the seat row letter: \n");                                                     // Gather's user input for seat row letter
    scanf(" %c ", &userChar);
    
-   if (userChar > 'K' || userChar == '@' || userChar == '$') {      // Checks row availability 
+   if (userChar > 'K' || userChar == '@' || userChar == '$') {                                  // Determines validity of row, makes sure user input for row is A-K
       printf("Not a valid row!\n");
       checkRow = 0;
    }
    
-   if (checkRow == 1) {
+   if (checkRow == 1) {                                                                         // Gather's user input for seat number only if the row input is valid (*Note: End bracket for the if-statement is at the end of the code, right before return 0;) 
       printf("Enter the seat number: \n");
       scanf("%d", &userInt);
    
   
-  if (userInt <= 0 || userInt > 16) {   //Checks seat number
+  if (userInt <= 0 || userInt > 16) {                                                           // Validates seat number from user input, makes sure seat number is between 1-16
       printf("Not a valid seat number!\n");  
    }
-   else if (userChar == 'A' || userChar == 'D' || userChar == 'J' || userChar == 'G') {
+         
+   else if (userChar == 'A' || userChar == 'D' || userChar == 'J' || userChar == 'G') {         // If row is a valid row & seat number, if-statement initiates scan for A, D, J, or G from user input (userChar) 
       
-      if (userInt%2 == 0) {
-            if (userInt <= 9) {
+      if (userInt%2 == 0) {                                                                     // If-statement determines if seat number is even, signifying unavailability of seats by molular operator (%2). Modular operator is based off of patterns of A, D, J, and G.
+            if (userInt <= 9) {                                                                 // If-statement adds a 0 in front of an integer less than 10, 0 in front of integer represents seat number (ex. A01). 
                printf("Seat %c0%d is taken!", userChar, userInt);
-            } else {
+            } else {                                                                            // If the user input for seat number is >9, then the 0 is taken off from the inputted integer value
                printf("Seat %c%d is taken!", userChar, userInt); 
             }
       } else {
-            if (userInt <= 9) {
+            if (userInt <= 9) {                                                                  
                printf("Seat %c0%d is open!", userChar, userInt);
             } else {
-               printf("Seat %c%d is open!", userChar, userInt);
+               printf("Seat %c%d is open!", userChar, userInt);                                 
             }
       }
    } 
    
-   else if (userChar == 'B' || userChar == 'E' || userChar == 'H' || userChar == 'K') {
-      if (userInt%3 == 0) {
-         if (userInt <= 9) {
+   else if (userChar == 'B' || userChar == 'E' || userChar == 'H' || userChar == 'K') {         // If-statement initiates scan for B, E, H, or K after scanning for A, D, J, G from user input (userChar)     
+      
+      if (userInt%3 == 0) {                                                                     // If-statement determines if seat number is divisible by 3, signifying unavailability of seats by molular operator (%3). If the seat number is divisible by 3, then the seat is open. Modular operator is based off of patterns of B, E, H, and K.
+         if (userInt <= 9) {	                                                                           
                printf("Seat %c0%d is open!", userChar, userInt);
             } else {
-               printf("Seat %c%d is open!", userChar, userInt); 
+               printf("Seat %c%d is open!", userChar, userInt);                                 
             }
       } else {
             if (userInt <= 9) {
@@ -75,8 +87,9 @@
       }
    } 
    
-   else if (userChar == 'C' || userChar == 'F' || userChar == 'I') {
-      if (userInt == 1 || userInt == 4 || userInt == 5 || userInt == 8 || userInt == 9 || userInt == 12 || userInt == 13 || userInt == 16 ) {
+   else if (userChar == 'C' || userChar == 'F' || userChar == 'I') {                            // If-statement initiates scan for C, F, or I after scanning for A, D, J, G, B, E, H, K from user input (userChar)
+         
+      if (userInt == 1 || userInt == 4 || userInt == 5 || userInt == 8 || userInt == 9 || userInt == 12 || userInt == 13 || userInt == 16 ) {         // If user inputs a number specified (1, 4, 5, 8, 9, 12, 13, 16) then the seat is open in rows C, F, and I. 
          if (userInt <= 9) {
             printf("Seat %c0%d is open!", userChar, userInt); 
          } else {
