6df98cb2c5233b5a93c16da647b843997af19b04
JimPalomo <43157036+JimPalomo@users.noreply.github.com>
2019-02-18 13:39:05 -0600

    Create 3.6.2.c

---
+++ CS 107/3 Branches/3.6.2.c
@@ -0,0 +1,21 @@
+/*
+            Write an expression that prints "Eligible" if userAge is between 18 and 25 inclusive. 
+            Ex: 17 prints "Ineligible", 18 prints "Eligible". 
+*/
+
+#include <stdio.h>
+
+int main(void) {
+   int userAge;
+
+   scanf("%d", &userAge);
+
+   if ((userAge >= 18) && (userAge <=25)) {
+       printf("Eligible\n");
+   }
+   else {
+      printf("Ineligible\n");
+   }
+
+   return 0;
+}
