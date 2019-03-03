d20ec93d821917b9f862766f8a6c5b1329f573b9
JimPalomo <43157036+JimPalomo@users.noreply.github.com>
2019-03-03 15:28:20 -0600

    Update 4.2.1.c

--- CS 107/4 Loops/4.2 While loops/4.2.1.c
+++ CS 107/4 Loops/4.2 While loops/4.2.1.c
@@ -1,10 +1,6 @@
 /*
       Write an expression that executes the loop while the user enters a number greater than or equal to 0. 
-
-
-      Note: These activities may test code with different test values. This activity will perform three tests, with userNum initially 9 and user input of 5, 2, -1, then with userNum initially 0 and user input of -17, then with userNum initially -1. See "How to Use zyBooks". . 
-
-      Also note: If the submitted code has an infinite loop, the system will stop running the code after a few seconds, and report "Program end never reached." The system doesn't print the test case that caused the reported message. 
+      
 */
 
 #include <stdio.h>
