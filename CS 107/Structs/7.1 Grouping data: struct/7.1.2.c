25991360009a0450ce7048aa2c3abcde063d465f
JimPalomo <43157036+JimPalomo@users.noreply.github.com>
2019-03-25 21:10:24 -0500

    Create 7.1.2.c

---
+++ CS 107/Structs/7.1 Grouping data: struct/7.1.2.c
@@ -0,0 +1,22 @@
+/*
+      Write a statement to print the data members of InventoryTag. End with newline. Ex: if itemID is 314 and quantityRemaining is 500, print:
+      Inventory ID: 314, Qty: 500
+*/
+
+#include <stdio.h>
+
+typedef struct InventoryTag_struct {
+   int itemID;
+   int quantityRemaining;
+} InventoryTag;
+
+int main(void) {
+   InventoryTag redSweater;
+
+   redSweater.itemID = 314;
+   redSweater.quantityRemaining = 500;
+
+   printf("Inventory ID: %d, Qty: %d\n", redSweater.itemID, redSweater.quantityRemaining);
+
+   return 0;
+}
