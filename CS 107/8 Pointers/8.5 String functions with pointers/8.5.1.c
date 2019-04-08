31783f0c5c6c3d56020df83c5059ddffe3787684
JimPalomo <43157036+JimPalomo@users.noreply.github.com>
2019-04-08 18:53:36 -0500

    Create 8.5.1.c

---
+++ CS 107/8 Pointers/8.5 String functions with pointers/8.5.1.c
@@ -0,0 +1,27 @@
+/*
+      Assign searchResult with a pointer to any instance of searchChar in personName. Hint: Use strchr(). 
+*/
+
+#include <stdio.h>
+#include <string.h>
+
+int main(void) {
+   char personName[100] = "Albert Johnson";
+   char searchChar;
+   char* searchResult = NULL;
+
+   searchChar = 'J';
+
+   /* Your solution goes here  */
+   searchResult = strchr(personName, searchChar);
+
+
+   if (searchResult != NULL) {
+      printf("Character found.\n");
+   }
+   else {
+      printf("Character not found.\n");
+   }
+
+   return 0;
+}
