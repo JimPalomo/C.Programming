d483aa4942efd992b044721bb69edd32ae014e8a
JimPalomo <43157036+JimPalomo@users.noreply.github.com>
2019-04-17 20:56:24 -0500

    Create 9.3.2.c

---
+++ CS 107/9 Input - Output/9.3 Output formatting/9.3.2.c
@@ -0,0 +1,17 @@
+/*
+      Write a single statement that prints outsideTemperature with a + or - sign. End with newline. Sample output:
+      +103.500000
+*/
+
+#include <stdio.h>
+
+int main(void) {
+   double outsideTemperature;
+
+   outsideTemperature = 103.5;
+
+   /* Your solution goes here  */
+   printf("%+lf\n", outsideTemperature);
+
+   return 0;
+}
