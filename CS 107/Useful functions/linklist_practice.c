0d576d60c5ec2150dcf99ec896dd0816067fe488
JimPalomo <43157036+JimPalomo@users.noreply.github.com>
2019-05-05 12:23:50 -0500

    Create linklist_practice.c

---
+++ CS 107/Useful functions/linklist_practice.c
@@ -0,0 +1,55 @@
+// Prints Linklist & counts Nodes
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node_struct {
+ int values[5];
+ int sum;
+ int max;
+ struct Node_struct* next;
+} Node;
+
+int countNodes(Node* myNode){
+   // Sets count to 1 with the head node accounted for
+   int count = 1;
+   
+   // Traverses the linked list by starting at the head node 
+   Node* currNode = myNode;
+   
+   // Linked list is traversed and nodes are counted through the use of an increment count variable
+   while(currNode->next != NULL) {
+      count++;
+      currNode = currNode -> next;
+   }
+   
+   return count;
+}
+
+void Node_PrintList(Node* myNode){
+   Node* currHead = myNode;
+    
+   while (currHead != NULL){
+      printf("%d\n", currHead->values);
+      currHead = currHead->next;
+   }
+    
+   printf("\n");
+}
+
+
+int main() {
+ Node* myNode = (Node*)malloc(sizeof(Node));
+ myNode->next = (Node*)malloc(sizeof(Node));
+ myNode->next->next = (Node*)malloc(sizeof(Node));
+ myNode->next->next->next = (Node*)malloc(sizeof(Node));
+ myNode->next->next->next->next = NULL;
+
+myNode->next->values[3] = 3;
+
+Node_PrintList(myNode);
+
+printf("%d", countNodes(myNode));
+
+ return 0;
+}
