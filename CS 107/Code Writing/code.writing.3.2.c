95961f23ecde595bf46188ca51e712145159d0f4
JimPalomo <43157036+JimPalomo@users.noreply.github.com>
2019-05-07 18:46:58 -0500

    Create code.writing.3.2.c

---
+++ CS 107/Code Writing/code.writing.3.2.c
@@ -0,0 +1,24 @@
+// Prints GPA's less than 2.
+
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    double gpa = 0.0;
+    char studentID[10];
+    
+    File *fp;
+    
+    fp = fopen("ListGPA", "r");
+    
+    while(!(feof(fp))) {
+        
+        fscanf(fp, "%s %lf", studentID, &gpa);
+        
+        if(gpa < 2.0) {
+            printf("%s %d", studentID, gpa);
+        }
+    }
+    
+    return 0;
+}
